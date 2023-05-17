59ef8ad981238676fe62708ba682ea423a9eb62a
brunofborges <brunofreitas.ads@gmail.com>
2023-05-17 13:32:12 -0300

    Refactored master to be initial branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { addTodo, removeTodo, loadTodos, updateTodo } from './state/todos/todo.actions';
 import { Observable } from 'rxjs';
-import { selectAllTodos } from './state/todos/todo.selectors';
-import { AppState } from './state/app.state';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Todo } from './state/todo.model';
 import { faTrash, faTrashArrowUp } from '@fortawesome/free-solid-svg-icons';
@@ -19,18 +16,20 @@ export class AppComponent implements OnInit{
   faTrash = faTrash;
   faTrashArrowUp = faTrashArrowUp;
 
-  allTodos$: Observable<Todo[]> = this.store.select(selectAllTodos);
+  allTodos: Todo[] = [{
+    id: Date.now().toString(),
+    text: 'Learn Angular',
+    completed: false
+  }]
   form!: FormGroup;
 
-  constructor(private store: Store<AppState>) {}
+  constructor() {}
 
   ngOnInit(): void {
     document.addEventListener("DOMContentLoaded", function () {
       const contentElement = document.getElementById("content")!;
       contentElement.classList.add("grow-animation");
     });
-
-    this.store.dispatch(loadTodos());
     this.form = new FormGroup({
       content: new FormControl('', [Validators.maxLength(30)])
     })
@@ -39,16 +38,15 @@ export class AppComponent implements OnInit{
   addTodo() {
     if (this.form.invalid) return;
     if(this.form.value.content.length == '') return;
-    this.store.dispatch(addTodo({ content: this.form.value.content }));
     this.form.reset();
   }
 
   removeTodo(todo: Todo) {
-    this.store.dispatch(removeTodo({ id: todo.id }));
+
   }
 
   updateTodo(todo: Todo) {
-    this.store.dispatch(updateTodo({ id: todo.id, completed: !todo.completed }));
+
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-
 import { AppComponent } from './app.component';
-import { todoReducer } from './state/todos/todo.reducer';
-import { TodoEffects } from './state/todos/todo.effects';
 import { TodoService } from './services/todo.service';
 import { NeonButtonComponent } from './components/neon-button.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -20,8 +15,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     BrowserModule,
     ReactiveFormsModule,
     FontAwesomeModule,
-    StoreModule.forRoot({ todos: todoReducer }) ,
-    EffectsModule.forRoot([TodoEffects])
   ],
   providers: [
     TodoService
